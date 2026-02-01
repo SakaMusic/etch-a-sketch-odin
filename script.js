de74144db35b0b2258b64dda5f31293abf7a6007
SakaMusic <wshbenson@gmail.com>
2026-02-01 09:14:12 -0700

    Added button functionality to change pixel count
    added destroyPixels func and createPixels to
    handle changing the amount of pixels in the
    etch-a-sketch

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+const gridHeight = 800;
+const gridWidth = 800;
+
+const buttonContainer = document.querySelector("#buttonContainer");
+let rowAndCol = 64;
+createPixels();
+
+buttonContainer.addEventListener('click', event => {
+    const buttonID = event.target.id;
+    switch (buttonID) {
+        case 'sixteen':
+            rowAndCol = 16;
+            break;
+        case 'thirtyTwo':
+            rowAndCol = 32;
+            break;
+        case 'sixtyFour':
+            rowAndCol = 64;
+            break;
+    }
+    console.log(rowAndCol);
+    destroyPixels()
+    createPixels();
+});
+
 function createDiv() {
     div = document.createElement('div');
-    div.style.height = '50px';
-    div.style.width = '50px';
+    div.style.height = `${gridHeight/rowAndCol}px`;
+    div.style.width = `${gridWidth/rowAndCol}px`;
     div.style.border = '0px';
     div.classList.add('pixel')
     return div;
@@ -9,6 +34,23 @@ function createDiv() {
 
 divContainer = document.querySelector("#divContainer")
 
-for (i = 0; i < 256; i++) {
-    divContainer.append(createDiv())
+// pixelCount = getGridNum();
+// pixelCount = pixelCount * pixelCount
+// console.log(pixelCount)
+function destroyPixels() {
+    if (divContainer.childElementCount > 0) {
+        while (divContainer.firstChild) {
+            divContainer.removeChild(divContainer.firstChild);
+        }
+    }
+}
+
+
+function createPixels() {
+    let pixels = rowAndCol;
+    pixels = pixels * pixels;
+
+    for (i = 0; i < pixels; i++) {
+        divContainer.append(createDiv())
+}
 }
