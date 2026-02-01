a3f4558a480c2688f6e1f156448a39fc16ee979f
SakaMusic <wshbenson@gmail.com>
2026-02-01 11:38:17 -0700

    Added color picker and eraser to change colors

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const gridHeight = 800;
 const gridWidth = 800;
 
+const divContainer = document.querySelector("#divContainer");
 const buttonContainer = document.querySelector("#buttonContainer");
-let rowAndCol = 64;
+const colorContainer = document.querySelector('#colorContainer');
+let rowAndCol = 16;
+let currentColor = 'black';
+
+let isMouseDown = false;
+document.body.addEventListener('mousedown', () => {
+    isMouseDown = true;
+});
+document.body.addEventListener('mouseup', () => {
+    isMouseDown = false;
+});
+
 createPixels();
 
 buttonContainer.addEventListener('click', event => {
+    // if (!event.target.classList.contains('.grid-button')) {
+    //     return;
+    // }
     const buttonID = event.target.id;
     switch (buttonID) {
         case 'sixteen':
@@ -23,20 +38,53 @@ buttonContainer.addEventListener('click', event => {
     createPixels();
 });
 
+colorContainer.addEventListener('click', event => {
+    // if (!event.target.classList.contains('.color-button')) return;
+    const colorButtonID = event.target.id;
+    switch (colorButtonID) {
+        case 'red':
+            currentColor = 'red';
+            break;
+        case 'green':
+            currentColor = 'green';
+            break;
+        case 'blue':
+            currentColor = 'blue';
+            break;
+        case 'yellow':
+            currentColor = 'yellow';
+            break;
+        case 'orange':
+            currentColor = 'orange';
+            break;
+        case 'purple':
+            currentColor = 'purple';
+            break;
+        case 'black':
+            currentColor = 'black';
+            break;
+        case 'white':
+            currentColor = 'white'
+            break
+    }
+})
+
 function createDiv() {
-    div = document.createElement('div');
+    let div = document.createElement('div');
     div.style.height = `${gridHeight/rowAndCol}px`;
     div.style.width = `${gridWidth/rowAndCol}px`;
     div.style.border = '0px';
     div.classList.add('pixel')
+    div.addEventListener('mousedown', () => colorDiv(div));
+    div.addEventListener('mouseover', () => {
+        if (isMouseDown) {
+            colorDiv(div);
+        }
+    });
     return div;
 }
 
-divContainer = document.querySelector("#divContainer")
 
-// pixelCount = getGridNum();
-// pixelCount = pixelCount * pixelCount
-// console.log(pixelCount)
 function destroyPixels() {
     if (divContainer.childElementCount > 0) {
         while (divContainer.firstChild) {
@@ -53,4 +101,8 @@ function createPixels() {
     for (i = 0; i < pixels; i++) {
         divContainer.append(createDiv())
 }
+}
+
+function colorDiv(element) {
+    element.style.backgroundColor = currentColor;
 }
